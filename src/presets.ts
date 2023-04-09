4ed51ce48790502c7a2a0e27bb54077e92b9aa2b
Jeffrey Davidsz <jeffrey.davidsz@vicreo.eu>
2023-04-09 17:31:08 +0200

    add ZoomISO presets

--- src/presets.ts
+++ src/presets.ts
@@ -1055,11 +1055,11 @@ export function GetPresetList(
 		feedbacks: [],
 	}
 	/**
-	 * ZoomISO Output Actions
+	 * ZoomISO Selections
 	 */
 	presets[`Apply_Output`] = {
 		type: 'button',
-		category: 'ZoomISO Output Actions',
+		category: 'ZoomISO Selections',
 		name: `Apply_Output`,
 		style: {
 			text: `Apply Output`,
@@ -1072,7 +1072,7 @@ export function GetPresetList(
 	}
 	presets[`Apply_Outputs`] = {
 		type: 'button',
-		category: 'ZoomISO Output Actions',
+		category: 'ZoomISO Selections',
 		name: `Apply_Outputs`,
 		style: {
 			text: `Apply Outputs`,
@@ -1085,7 +1085,7 @@ export function GetPresetList(
 	}
 	presets[`Apply_Channel`] = {
 		type: 'button',
-		category: 'ZoomISO Output Actions',
+		category: 'ZoomISO Selections',
 		name: `Apply_Channel`,
 		style: {
 			text: `Apply Channel`,
@@ -1098,12 +1098,12 @@ export function GetPresetList(
 	}
 	for (
 		let index = 1;
-		index < (Object.keys(ZoomAudioRoutingData).length === 0 ? 9 : (Object.keys(ZoomAudioRoutingData).length + 1));
+		index < (Object.keys(ZoomAudioRoutingData).length === 0 ? 9 : Object.keys(ZoomAudioRoutingData).length + 1);
 		index++
 	) {
 		presets[`Select_Output_${index}`] = {
 			type: 'button',
-			category: 'ZoomISO Output Actions',
+			category: 'ZoomISO Selections',
 			name: `Select Output ${index}`,
 			style: {
 				text: `Select Output ${index}`,
@@ -1139,7 +1139,7 @@ export function GetPresetList(
 	for (let index = 1; index < 9; index++) {
 		presets[`Select_Audio_Channel ${index}`] = {
 			type: 'button',
-			category: 'ZoomISO Output Actions',
+			category: 'ZoomISO Selections',
 			name: `Audio\nChannel ${index}`,
 			style: {
 				text: `Audio\nChannel ${index}`,
@@ -1174,7 +1174,7 @@ export function GetPresetList(
 	}
 	presets[`Select_Channel`] = {
 		type: 'button',
-		category: 'ZoomISO Output Actions',
+		category: 'ZoomISO Selections',
 		name: `Select_Channel`,
 		style: {
 			text: `Select Channel`,
@@ -1183,6 +1183,270 @@ export function GetPresetList(
 			bgcolor: combineRgb(141, 218, 77),
 		},
 		steps: [{ down: [{ actionId: ActionId.selectAudioChannel, options: { output: 1 } }], up: [] }],
+		feedbacks: [],
+	}
+
+	/**
+	 * ZoomISO Actions
+	 */
+	presets[`set_user_to_output`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_user_to_output`,
+		style: {
+			text: `Set User to Output`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.outputISO, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`add_Output`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `add_Output`,
+		style: {
+			text: `Add Output`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.addOutput, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`delete_Output`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `delete_Output`,
+		style: {
+			text: `Delete Output`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.deleteOutput, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`disable_Output`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `disable_Output`,
+		style: {
+			text: `Disable Output`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.disableOutput, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`enable_Output`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `enable_Output`,
+		style: {
+			text: `Enable Output`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.enableOutput, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_count`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_count`,
+		style: {
+			text: `set Output Count`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputCount, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_embedded_audio`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_embedded_audio`,
+		style: {
+			text: `Set Output Embedded Audio`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputEmbeddedAudio, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_name`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_name`,
+		style: {
+			text: `Set Output Name`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputName, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_selection`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_selection`,
+		style: {
+			text: `Set Output Selection`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputSelection, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_mode`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_mode`,
+		style: {
+			text: `Set Output Mode`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputMode, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_type`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_type`,
+		style: {
+			text: `Set Output Type`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputType, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`standby_iso_engine`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `standby_iso_engine`,
+		style: {
+			text: `Standby ISO Engine`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.standbyISOEngine, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`start_iso_engine`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `start_iso_engine`,
+		style: {
+			text: `Start ISO Engine`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.startISOEngine, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`stop_iso_engine`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `stop_iso_engine`,
+		style: {
+			text: `Stop ISO Engine`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.stopISOEngine, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_video_loss_mode`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_video_loss_mode`,
+		style: {
+			text: `Set Video Loss Mode`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setVideoLossMode, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_user_to_audio_channel`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_user_to_audio_channel`,
+		style: {
+			text: `Set User to Audio Channel`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.audioISO, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_audio_gain_reduction`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_audio_gain_reduction`,
+		style: {
+			text: `Set Audio Gain Reduction`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setAudioGainReduction, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_audio_selection`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_audio_selection`,
+		style: {
+			text: `Set Audio Selection`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setAudioSelection, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_output_embedded_audio`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_output_embedded_audio`,
+		style: {
+			text: `Set Output Embedded Audio`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setOutputEmbeddedAudio, options: {} }], up: [] }],
+		feedbacks: [],
+	}
+	presets[`set_audio_mode`] = {
+		type: 'button',
+		category: 'ZoomISO Actions',
+		name: `set_audio_mode`,
+		style: {
+			text: `Set Audio Mode`,
+			size: '14',
+			color: combineRgb(0, 0, 0),
+			bgcolor: combineRgb(141, 218, 77),
+		},
+		steps: [{ down: [{ actionId: ActionId.setAudioMode, options: {} }], up: [] }],
 		feedbacks: [],
 	}
 
