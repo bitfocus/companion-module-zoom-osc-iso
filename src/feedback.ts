fecdb753b1cf3d5f3b994643985653d058ff2931
Justin James <digitaldrummerj@gmail.com>
2023-04-27 20:24:25 -0700

    update: feedback to advanced feedback and setting image to show all states

--- src/feedback.ts
+++ src/feedback.ts
@@ -2,10 +2,12 @@ import {
 	CompanionFeedbackDefinitions,
 	combineRgb,
 	CompanionFeedbackDefinition,
-	InputValue,
+	// InputValue,
+	CompanionAdvancedFeedbackResult,
 } from '@companion-module/base'
 import { ZoomConfig } from './config'
 import { InstanceBaseExt, userExist } from './utils'
+const { images } = require('./images') // eslint-disable-line
 
 export enum FeedbackId {
 	selectionMethod = 'selection_Method',
@@ -34,68 +36,160 @@ export enum feedbackType {
 	cameraOff = 7,
 }
 
+type State = [boolean, boolean, boolean, boolean]
+interface StateMachine {
+	states: State[]
+	images: Record<string, string>
+	currentState: State
+}
+
+const stateMachine: StateMachine = {
+	states: [
+		[false, false, false, false],
+		[false, false, false, true],
+		[false, false, true, false],
+		[false, false, true, true],
+		[false, true, false, false],
+		[false, true, false, true],
+		[false, true, true, false],
+		[false, true, true, true],
+		[true, false, false, false],
+		[true, false, false, true],
+		[true, false, true, false],
+		[true, false, true, true],
+		[true, true, false, false],
+		[true, true, false, true],
+		[true, true, true, false],
+		[true, true, true, true],
+	],
+	images: {
+		'false,false,false,false': images.LG1,
+		'false,false,false,true': images.LG2,
+		'false,false,true,false': images.LG3,
+		'false,false,true,true': images.LG4,
+		'false,true,false,false': images.LG5,
+		'false,true,false,true': images.LG6,
+		'false,true,true,false': images.LG7,
+		'false,true,true,true': images.LG8,
+		'true,false,false,false': images.LG9,
+		'true,false,false,true': images.LG10,
+		'true,false,true,false': images.LG11,
+		'true,false,true,true': images.LG12,
+		'true,true,false,false': images.LG13,
+		'true,true,false,true': images.LG14,
+		'true,true,true,false': images.LG15,
+		'true,true,true,true': images.LG16,
+	},
+	currentState: [false, false, false, false],
+}
+
+function getImageForState(state: State): string {
+	const stateString = state.toString()
+	return stateMachine.images[stateString]
+}
+
 export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFeedbackDefinitions {
 	const CHOICES_GROUPS = instance.ZoomGroupData.length === 0 ? [{ id: '0', label: 'no position' }] : []
 	for (let index = 0; index < instance.ZoomGroupData.length; index++) {
 		CHOICES_GROUPS.push({ id: index.toString(), label: instance.ZoomGroupData[index].groupName })
 	}
 
-	function feedbackResults(type: InputValue | undefined, zoomID: number) {
-		if (type === undefined) {
-			return false
-		}
+	// function feedbackResults(type: InputValue | undefined, zoomID: number): boolean {
+	// 	if (type === undefined) {
+	// 		return false
+	// 	}
 
-		switch (type) {
-			case feedbackType.micLive:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return instance.ZoomUserData[zoomID].mute === false
+	// 	switch (type) {
+	// 		case feedbackType.micLive:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return instance.ZoomUserData[zoomID].mute === false
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.camera:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return instance.ZoomUserData[zoomID].videoOn === true
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.handRaised:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return instance.ZoomUserData[zoomID].handRaised === true
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.micOff:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return instance.ZoomUserData[zoomID].mute === true
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.cameraOff:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return instance.ZoomUserData[zoomID].videoOn === false
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.handLowered:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return instance.ZoomUserData[zoomID].handRaised === false
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.activeSpeaker:
+	// 			if (userExist(zoomID, instance.ZoomUserData)) {
+	// 				return (
+	// 					instance.ZoomClientDataObj.activeSpeaker === instance.ZoomUserData[zoomID].userName &&
+	// 					instance.ZoomUserData[zoomID].mute === false
+	// 				)
+	// 			}
+
+	// 			return false
+	// 		case feedbackType.selected:
+	// 			return instance.ZoomClientDataObj.selectedCallers.find((element: number) => element === zoomID) ? true : false
+
+	// 		default:
+	// 			return false
+	// 	}
+	// }
+
+	function feedbackResultsAdvanced(zoomID: number): CompanionAdvancedFeedbackResult {
+		// instance.log('debug', `feedback feedbackResultsAdvanced: zoomID: ${zoomID}`)
+		if (userExist(zoomID, instance.ZoomUserData)) {
+			const stateArray: State = [
+				instance.ZoomUserData[zoomID].mute || false,
+				instance.ZoomUserData[zoomID].videoOn || false,
+				instance.ZoomUserData[zoomID].handRaised || false,
+				instance.ZoomClientDataObj.activeSpeaker === instance.ZoomUserData[zoomID].userName || false,
+			]
+
+			// instance.log(
+			// 	'debug',
+			// 	`feedback feedbackResultsAdvanced: userData - ${JSON.stringify(
+			// 		instance.ZoomUserData[zoomID]
+			// 	)}, activeSpeaker - ${JSON.stringify(instance.ZoomClientDataObj.activeSpeaker)}, (state - ${JSON.stringify(
+			// 		stateArray
+			// 	)})`
+			// )
+
+			const stateIndex = stateMachine.states.findIndex((state) => state.toString() === stateArray.toString())
+			// instance.log('debug', `feedback feedbackResultsAdvanced: stateIndex: ${stateIndex}`)
+			stateMachine.currentState = stateMachine.states[stateIndex]
+			const image = getImageForState(stateMachine.currentState)
+
+			if (instance.ZoomClientDataObj.selectedCallers.find((element: number) => element === zoomID)) {
+				return {
+					png64: image,
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(255, 255, 0),
 				}
+			}
 
-				return false
-			case feedbackType.camera:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return instance.ZoomUserData[zoomID].videoOn === true
-				}
-
-				return false
-			case feedbackType.handRaised:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return instance.ZoomUserData[zoomID].handRaised === true
-				}
-
-				return false
-			case feedbackType.micOff:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return instance.ZoomUserData[zoomID].mute === true
-				}
-
-				return false
-			case feedbackType.cameraOff:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return instance.ZoomUserData[zoomID].videoOn === false
-				}
-
-				return false
-			case feedbackType.handLowered:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return instance.ZoomUserData[zoomID].handRaised === false
-				}
-
-				return false
-			case feedbackType.activeSpeaker:
-				if (userExist(zoomID, instance.ZoomUserData)) {
-					return (
-						instance.ZoomClientDataObj.activeSpeaker === instance.ZoomUserData[zoomID].userName &&
-						instance.ZoomUserData[zoomID].mute === false
-					)
-				}
-
-				return false
-			case feedbackType.selected:
-				return instance.ZoomClientDataObj.selectedCallers.find((element: number) => element === zoomID) ? true : false
-
-			default:
-				return false
+			return {
+				png64: image,
+			}
+		} else {
+			return {}
 		}
 	}
 	const feedbacks: { [id in FeedbackId]: CompanionFeedbackDefinition | undefined } = {
@@ -128,12 +222,13 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 			},
 		},
 		[FeedbackId.groupBased]: {
-			type: 'boolean',
+			// type: 'boolean',
+			type: 'advanced',
 			name: 'Group based feedback',
 			description: 'Position 1 - 999',
-			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
-			},
+			// defaultStyle: {
+			// 	bgcolor: combineRgb(255, 0, 0),
+			// },
 			options: [
 				{
 					type: 'dropdown',
@@ -150,40 +245,43 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 					min: 1,
 					max: 999,
 				},
-				{
-					type: 'dropdown',
-					label: 'Type of feedback',
-					id: 'type',
-					default: feedbackType.selected,
-					choices: [
-						{ id: feedbackType.selected, label: 'Selected' },
-						{ id: feedbackType.micLive, label: 'Mic Live' },
-						{ id: feedbackType.handRaised, label: 'Hand Raised' },
-						{ id: feedbackType.camera, label: 'Camera on' },
-						{ id: feedbackType.micOff, label: 'Mic Off' },
-						{ id: feedbackType.handLowered, label: 'Hand Lowered' },
-						{ id: feedbackType.cameraOff, label: 'Camera Off' },
-						{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
-					],
-				},
+				// {
+				// 	type: 'dropdown',
+				// 	label: 'Type of feedback',
+				// 	id: 'type',
+				// 	default: feedbackType.selected,
+				// 	choices: [
+				// 		{ id: feedbackType.selected, label: 'Selected' },
+				// 		{ id: feedbackType.micLive, label: 'Mic Live' },
+				// 		{ id: feedbackType.handRaised, label: 'Hand Raised' },
+				// 		{ id: feedbackType.camera, label: 'Camera on' },
+				// 		{ id: feedbackType.micOff, label: 'Mic Off' },
+				// 		{ id: feedbackType.handLowered, label: 'Hand Lowered' },
+				// 		{ id: feedbackType.cameraOff, label: 'Camera Off' },
+				// 		{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
+				// 	],
+				// },
 			],
 			callback: (feedback) => {
 				if (instance.ZoomGroupData[feedback.options.group as number].users[(feedback.options.position as number) - 1]) {
 					const zoomID =
 						instance.ZoomGroupData[feedback.options.group as number].users[(feedback.options.position as number) - 1]
 							.zoomID
-					return feedbackResults(feedback.options.type, zoomID)
+					// return feedbackResults(feedback.options.type, zoomID)
+					return feedbackResultsAdvanced(zoomID)
 				}
-				return false
+				// return false
+				return {}
 			},
 		},
 		[FeedbackId.indexBased]: {
-			type: 'boolean',
+			// type: 'boolean',
+			type: 'advanced',
 			name: 'Participant position based feedback',
 			description: 'Feedback based on index of the participant',
-			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
-			},
+			// defaultStyle: {
+			// 	bgcolor: combineRgb(255, 0, 0),
+			// },
 			options: [
 				{
 					type: 'number',
@@ -193,39 +291,42 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 					min: 1,
 					max: 999,
 				},
-				{
-					type: 'dropdown',
-					label: 'Type of feedback',
-					id: 'type',
-					default: feedbackType.selected,
-					choices: [
-						{ id: feedbackType.selected, label: 'Selected' },
-						{ id: feedbackType.micLive, label: 'Mic Live' },
-						{ id: feedbackType.handRaised, label: 'Hand Raised' },
-						{ id: feedbackType.camera, label: 'Camera on' },
-						{ id: feedbackType.micOff, label: 'Mic Off' },
-						{ id: feedbackType.handLowered, label: 'Hand Lowered' },
-						{ id: feedbackType.cameraOff, label: 'Camera Off' },
-						{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
-					],
-				},
+				// {
+				// 	type: 'dropdown',
+				// 	label: 'Type of feedback',
+				// 	id: 'type',
+				// 	default: feedbackType.selected,
+				// 	choices: [
+				// 		{ id: feedbackType.selected, label: 'Selected' },
+				// 		{ id: feedbackType.micLive, label: 'Mic Live' },
+				// 		{ id: feedbackType.handRaised, label: 'Hand Raised' },
+				// 		{ id: feedbackType.camera, label: 'Camera on' },
+				// 		{ id: feedbackType.micOff, label: 'Mic Off' },
+				// 		{ id: feedbackType.handLowered, label: 'Hand Lowered' },
+				// 		{ id: feedbackType.cameraOff, label: 'Camera Off' },
+				// 		{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
+				// 	],
+				// },
 			],
 			callback: (feedback) => {
 				if (instance.ZoomVariableLink[(feedback.options.position as number) - 1]) {
 					const zoomID = instance.ZoomVariableLink[(feedback.options.position as number) - 1].zoomId
 
-					return feedbackResults(feedback.options.type, zoomID)
+					// return feedbackResults(feedback.options.type, zoomID)
+					return feedbackResultsAdvanced(zoomID)
 				}
-				return false
+				// return false
+				return {}
 			},
 		},
 		[FeedbackId.userNameBased]: {
-			type: 'boolean',
+			// type: 'boolean',
+			type: 'advanced',
 			name: 'Username based feedback',
 			description: 'type in username',
-			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
-			},
+			// defaultStyle: {
+			// 	bgcolor: combineRgb(255, 0, 0),
+			// },
 			options: [
 				{
 					type: 'textinput',
@@ -233,22 +334,22 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 					id: 'name',
 					default: '',
 				},
-				{
-					type: 'dropdown',
-					label: 'Type of feedback',
-					id: 'type',
-					default: feedbackType.selected,
-					choices: [
-						{ id: feedbackType.selected, label: 'Selected' },
-						{ id: feedbackType.micLive, label: 'Mic Live' },
-						{ id: feedbackType.handRaised, label: 'Hand Raised' },
-						{ id: feedbackType.camera, label: 'Camera on' },
-						{ id: feedbackType.micOff, label: 'Mic Off' },
-						{ id: feedbackType.handLowered, label: 'Hand Lowered' },
-						{ id: feedbackType.cameraOff, label: 'Camera Off' },
-						{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
-					],
-				},
+				// {
+				// 	type: 'dropdown',
+				// 	label: 'Type of feedback',
+				// 	id: 'type',
+				// 	default: feedbackType.selected,
+				// 	choices: [
+				// 		{ id: feedbackType.selected, label: 'Selected' },
+				// 		{ id: feedbackType.micLive, label: 'Mic Live' },
+				// 		{ id: feedbackType.handRaised, label: 'Hand Raised' },
+				// 		{ id: feedbackType.camera, label: 'Camera on' },
+				// 		{ id: feedbackType.micOff, label: 'Mic Off' },
+				// 		{ id: feedbackType.handLowered, label: 'Hand Lowered' },
+				// 		{ id: feedbackType.cameraOff, label: 'Camera Off' },
+				// 		{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
+				// 	],
+				// },
 			],
 			callback: async (feedback, context) => {
 				const name = await context.parseVariablesInString(feedback.options.name as string)
@@ -257,19 +358,22 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 					if (iterator.userName === name) {
 						zoomID = iterator.zoomId
 
-						return feedbackResults(feedback.options.type, zoomID)
+						// return feedbackResults(feedback.options.type, zoomID)
+						return feedbackResultsAdvanced(zoomID)
 					}
 				}
-				return false
+				// return false
+				return {}
 			},
 		},
 		[FeedbackId.galleryBased]: {
-			type: 'boolean',
+			// type: 'boolean',
+			type: 'advanced',
 			name: 'Gallery based feedback',
 			description: 'Position 1 - 49',
-			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
-			},
+			// defaultStyle: {
+			// 	bgcolor: combineRgb(255, 0, 0),
+			// },
 			options: [
 				{
 					type: 'number',
@@ -279,22 +383,22 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 					min: 1,
 					max: 49,
 				},
-				{
-					type: 'dropdown',
-					label: 'Type of feedback',
-					id: 'type',
-					default: feedbackType.selected,
-					choices: [
-						{ id: feedbackType.selected, label: 'Selected' },
-						{ id: feedbackType.micLive, label: 'Mic Live' },
-						{ id: feedbackType.handRaised, label: 'Hand Raised' },
-						{ id: feedbackType.camera, label: 'Camera on' },
-						{ id: feedbackType.micOff, label: 'Mic Off' },
-						{ id: feedbackType.handLowered, label: 'Hand Lowered' },
-						{ id: feedbackType.cameraOff, label: 'Camera Off' },
-						{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
-					],
-				},
+				// {
+				// 	type: 'dropdown',
+				// 	label: 'Type of feedback',
+				// 	id: 'type',
+				// 	default: feedbackType.selected,
+				// 	choices: [
+				// 		{ id: feedbackType.selected, label: 'Selected' },
+				// 		{ id: feedbackType.micLive, label: 'Mic Live' },
+				// 		{ id: feedbackType.handRaised, label: 'Hand Raised' },
+				// 		{ id: feedbackType.camera, label: 'Camera on' },
+				// 		{ id: feedbackType.micOff, label: 'Mic Off' },
+				// 		{ id: feedbackType.handLowered, label: 'Hand Lowered' },
+				// 		{ id: feedbackType.cameraOff, label: 'Camera Off' },
+				// 		{ id: feedbackType.activeSpeaker, label: 'Active Speaker' },
+				// 	],
+				// },
 			],
 			callback: (feedback) => {
 				if (instance.ZoomUserData[instance.ZoomClientDataObj.galleryOrder[(feedback.options.position as number) - 1]]) {
@@ -302,9 +406,11 @@ export function GetFeedbacks(instance: InstanceBaseExt<ZoomConfig>): CompanionFe
 						instance.ZoomUserData[instance.ZoomClientDataObj.galleryOrder[(feedback.options.position as number) - 1]]
 							.zoomId
 
-					return feedbackResults(feedback.options.type, zoomID)
+					// return feedbackResults(feedback.options.type, zoomID)
+					return feedbackResultsAdvanced(zoomID)
 				}
-				return false
+				// return false
+				return {}
 			},
 		},
 		[FeedbackId.engineState]: {
--- src/presets.ts
+++ src/presets.ts
@@ -1,7 +1,7 @@
 import { combineRgb, CompanionButtonPresetDefinition, CompanionPresetDefinitions } from '@companion-module/base'
 import { ActionId } from './actions'
 import { FeedbackId } from './feedback'
-const { images } = require('./images') // eslint-disable-line
+// const { images } = require('./images') // eslint-disable-line
 import {
 	padding,
 	userExist,
@@ -10,13 +10,13 @@ import {
 	ZoomUserDataInterface,
 } from './utils'
 
-enum feedbackType {
-	selected = 0,
-	micLive = 1,
-	handRaised = 2,
-	camera = 3,
-	activeSpeaker = 4,
-}
+// enum feedbackType {
+// 	selected = 0,
+// 	micLive = 1,
+// 	handRaised = 2,
+// 	camera = 3,
+// 	activeSpeaker = 4,
+// }
 
 interface CompanionPresetExt extends CompanionButtonPresetDefinition {
 	feedbacks: Array<
@@ -41,28 +41,28 @@ interface CompanionPresetDefinitionsExt {
 	[id: string]: CompanionPresetExt | undefined
 }
 
-interface FeedbackTypeOptions {
-	color?: number
-	bgcolor?: number
-	png64?: string
-}
-const participantFeedbackStyles: { [key: string]: FeedbackTypeOptions } = {
-	micLive: {
-		color: combineRgb(0, 0, 0),
-		bgcolor: combineRgb(255, 0, 0),
-	},
-	activeSpeaker: {
-		color: combineRgb(255, 255, 255),
-		bgcolor: combineRgb(0, 0, 255),
-	},
-	handRaised: {
-		png64: images.handRaised,
-	},
-	selected: {
-		color: combineRgb(0, 0, 0),
-		bgcolor: combineRgb(255, 255, 0),
-	},
-}
+// interface FeedbackTypeOptions {
+// 	color?: number
+// 	bgcolor?: number
+// 	png64?: string
+// }
+// const participantFeedbackStyles: { [key: string]: FeedbackTypeOptions } = {
+// 	micLive: {
+// 		color: combineRgb(0, 0, 0),
+// 		bgcolor: combineRgb(255, 0, 0),
+// 	},
+// 	activeSpeaker: {
+// 		color: combineRgb(255, 255, 255),
+// 		bgcolor: combineRgb(0, 0, 255),
+// 	},
+// 	handRaised: {
+// 		png64: images.handRaised,
+// 	},
+// 	selected: {
+// 		color: combineRgb(0, 0, 0),
+// 		bgcolor: combineRgb(255, 255, 0),
+// 	},
+// }
 
 export function GetPresetList(
 	ZoomGroupData: ZoomGroupDataInterface[],
@@ -104,35 +104,45 @@ export function GetPresetList(
 					feedbackId: FeedbackId.indexBased,
 					options: {
 						position: index,
-						type: feedbackType.micLive,
 					},
-					style: participantFeedbackStyles.micLive,
-				},
-				{
-					feedbackId: FeedbackId.indexBased,
-					options: {
-						position: index,
-						type: feedbackType.handRaised,
-						handRaised: 1,
+					style: {
+						color: combineRgb(0, 0, 0),
+						bgcolor: combineRgb(230, 230, 230),
 					},
-					style: participantFeedbackStyles.handRaised,
 				},
-				{
-					feedbackId: FeedbackId.indexBased,
-					options: {
-						position: index,
-						type: feedbackType.activeSpeaker,
-					},
-					style: participantFeedbackStyles.activeSpeaker,
-				},
-				{
-					feedbackId: FeedbackId.indexBased,
-					options: {
-						position: index,
-						type: feedbackType.selected,
-					},
-					style: participantFeedbackStyles.selected,
-				},
+				// {
+				// 	feedbackId: FeedbackId.indexBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.micLive,
+				// 	},
+				// 	style: participantFeedbackStyles.micLive,
+				// },
+				// {
+				// 	feedbackId: FeedbackId.indexBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.handRaised,
+				// 		handRaised: 1,
+				// 	},
+				// 	style: participantFeedbackStyles.handRaised,
+				// },
+				// {
+				// 	feedbackId: FeedbackId.indexBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.activeSpeaker,
+				// 	},
+				// 	style: participantFeedbackStyles.activeSpeaker,
+				// },
+				// {
+				// 	feedbackId: FeedbackId.indexBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.selected,
+				// 	},
+				// 	style: participantFeedbackStyles.selected,
+				// },
 			],
 		}
 	}
@@ -162,35 +172,45 @@ export function GetPresetList(
 					feedbackId: FeedbackId.galleryBased,
 					options: {
 						position: index,
-						type: feedbackType.micLive,
 					},
-					style: participantFeedbackStyles.micLive,
-				},
-				{
-					feedbackId: FeedbackId.galleryBased,
-					options: {
-						position: index,
-						type: feedbackType.handRaised,
+					style: {
+						color: combineRgb(0, 0, 0),
+						bgcolor: combineRgb(230, 230, 230),
 					},
-					style: participantFeedbackStyles.handRaised,
 				},
-				{
-					feedbackId: FeedbackId.galleryBased,
-					options: {
-						position: index,
-						type: feedbackType.activeSpeaker,
-					},
-					style: participantFeedbackStyles.activeSpeaker,
-				},
+				// {
+				// 	feedbackId: FeedbackId.galleryBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.micLive,
+				// 	},
+				// 	style: participantFeedbackStyles.micLive,
+				// },
+				// {
+				// 	feedbackId: FeedbackId.galleryBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.handRaised,
+				// 	},
+				// 	style: participantFeedbackStyles.handRaised,
+				// },
+				// {
+				// 	feedbackId: FeedbackId.galleryBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.activeSpeaker,
+				// 	},
+				// 	style: participantFeedbackStyles.activeSpeaker,
+				// },
 
-				{
-					feedbackId: FeedbackId.galleryBased,
-					options: {
-						position: index,
-						type: feedbackType.selected,
-					},
-					style: participantFeedbackStyles.selected,
-				},
+				// {
+				// 	feedbackId: FeedbackId.galleryBased,
+				// 	options: {
+				// 		position: index,
+				// 		type: feedbackType.selected,
+				// 	},
+				// 	style: participantFeedbackStyles.selected,
+				// },
 			],
 		}
 	}
@@ -299,34 +319,44 @@ export function GetPresetList(
 				feedbackId: FeedbackId.userNameBased,
 				options: {
 					name: '',
-					type: feedbackType.micLive,
 				},
-				style: participantFeedbackStyles.micLive,
-			},
-			{
-				feedbackId: FeedbackId.userNameBased,
-				options: {
-					name: '',
-					type: feedbackType.handRaised,
+				style: {
+					color: combineRgb(0, 0, 0),
+					bgcolor: combineRgb(230, 230, 230),
 				},
-				style: participantFeedbackStyles.handRaised,
 			},
-			{
-				feedbackId: FeedbackId.userNameBased,
-				options: {
-					name: '',
-					type: feedbackType.activeSpeaker,
-				},
-				style: participantFeedbackStyles.activeSpeaker,
-			},
-			{
-				feedbackId: FeedbackId.userNameBased,
-				options: {
-					name: '',
-					type: feedbackType.selected,
-				},
-				style: participantFeedbackStyles.selected,
-			},
+			// {
+			// 	feedbackId: FeedbackId.userNameBased,
+			// 	options: {
+			// 		name: '',
+			// 		type: feedbackType.micLive,
+			// 	},
+			// 	style: participantFeedbackStyles.micLive,
+			// },
+			// {
+			// 	feedbackId: FeedbackId.userNameBased,
+			// 	options: {
+			// 		name: '',
+			// 		type: feedbackType.handRaised,
+			// 	},
+			// 	style: participantFeedbackStyles.handRaised,
+			// },
+			// {
+			// 	feedbackId: FeedbackId.userNameBased,
+			// 	options: {
+			// 		name: '',
+			// 		type: feedbackType.activeSpeaker,
+			// 	},
+			// 	style: participantFeedbackStyles.activeSpeaker,
+			// },
+			// {
+			// 	feedbackId: FeedbackId.userNameBased,
+			// 	options: {
+			// 		name: '',
+			// 		type: feedbackType.selected,
+			// 	},
+			// 	style: participantFeedbackStyles.selected,
+			// },
 		],
 	}
 	// Multiple selection
@@ -489,38 +519,49 @@ export function GetPresetList(
 						options: {
 							group: index,
 							position: position,
-							type: feedbackType.micLive,
 						},
-						style: participantFeedbackStyles.micLive,
-					},
-					{
-						feedbackId: FeedbackId.groupBased,
-						options: {
-							group: index,
-							position: position,
-							type: feedbackType.handRaised,
+						style: {
+							color: combineRgb(0, 0, 0),
+							bgcolor: combineRgb(230, 230, 230),
 						},
-						style: participantFeedbackStyles.handRaised,
 					},
-					{
-						feedbackId: FeedbackId.groupBased,
-						options: {
-							group: index,
-							position: position,
-							type: feedbackType.activeSpeaker,
-						},
-						style: participantFeedbackStyles.activeSpeaker,
-					},
+					// {
+					// 	feedbackId: FeedbackId.groupBased,
+					// 	options: {
+					// 		group: index,
+					// 		position: position,
+					// 		type: feedbackType.micLive,
+					// 	},
+					// 	style: participantFeedbackStyles.micLive,
+					// },
+					// {
+					// 	feedbackId: FeedbackId.groupBased,
+					// 	options: {
+					// 		group: index,
+					// 		position: position,
+					// 		type: feedbackType.handRaised,
+					// 	},
+					// 	style: participantFeedbackStyles.handRaised,
+					// },
+					// {
+					// 	feedbackId: FeedbackId.groupBased,
+					// 	options: {
+					// 		group: index,
+					// 		position: position,
+					// 		type: feedbackType.activeSpeaker,
+					// 	},
+					// 	style: participantFeedbackStyles.activeSpeaker,
+					// },
 
-					{
-						feedbackId: FeedbackId.groupBased,
-						options: {
-							group: index,
-							position: position,
-							type: feedbackType.selected,
-						},
-						style: participantFeedbackStyles.selected,
-					},
+					// {
+					// 	feedbackId: FeedbackId.groupBased,
+					// 	options: {
+					// 		group: index,
+					// 		position: position,
+					// 		type: feedbackType.selected,
+					// 	},
+					// 	style: participantFeedbackStyles.selected,
+					// },
 				],
 			}
 		}
